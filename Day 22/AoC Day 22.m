2a0e0a823d1420a4076a124155a44f049c81a552
HiggstonRainbird <fberg.david@gmail.com>
2023-12-22 01:34:35 -0500

    Minor revision to Day 22.

--- Day 22/AoC Day 22.m
+++ Day 22/AoC Day 22.m
@@ -1,11 +1,11 @@
 (* ::Package:: *)
 
 (* ::Text:: *)
-(*Written December 18th, 2023.*)
+(*Written December 22nd, 2023.*)
 
 (*Import*)
 
-day = 18;
+day = 22;
 inputPath = 
   FileNameJoin[{NotebookDirectory[], 
     "Day" <> ToString[day] <> "Input.txt"}];
@@ -19,28 +19,52 @@ toExpression[inputText_] :=
    inputText,
    {Depth[inputText] - 1, Depth[inputText]}];
 
-input = toExpression[Import[inputPath, "Table"]];
+input = toExpression[
+   Partition[StringSplit[#, "," | "~"], 3] & /@ 
+    StringSplit[Import[inputPath], "\n"]];
 
-(*Part 1*)
+(*Setup*)
 
-directions = 
-  Thread[{"R", "D", "L", "U"} -> {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}];
-instructions = {#[[2]], #[[1]] /. directions} & /@ input;
+cubes = SortBy[input, #[[1, -1]] &];
 
-trench = FoldList[#1 + Times @@ #2 &, {0, 0}, instructions];
-(Area[Polygon[trench]] + Perimeter[Polygon[trench]]/2 + 1)
+ClearAll@filled;
+filled[{x_, y_, z_}] := \[Infinity];
+filled[{x_, y_, z_ /; z < 1}] := -1;
+
+newCubes = CreateDataStructure["DynamicArray"];
+Do[
+  newC = cubes[[c]];
+  While[
+   tmp = (# - {0, 0, 1}) & /@ newC;
+   AllTrue[Tuples[Range @@@ Transpose[tmp]], 
+    filled[#] == \[Infinity] &],
+   newC = tmp];
+  Do[filled[t] = c, {t, Tuples[Range @@@ Transpose[newC]]}];
+  newCubes["Append", newC];
+  , {c, Length[cubes]}];
+
+g = Graph[
+   Union[Flatten[
+     Table[
+      # -> c & /@ Select[Table[
+         filled[t - {0, 0, 1}],
+         {t, Tuples[Range @@@ Transpose[newCubes["Part", c]]]}],
+        # != \[Infinity] \[And] # != c &],
+      {c, newCubes["Length"]}]]]];
+
+(* Part 1 *)
+
+Count[
+ VertexList[g],
+ _?(AllTrue[
+     VertexList[VertexOutComponentGraph[g, #, {1}]],
+     VertexInDegree[g, #] > 1 &] &)]
 
 (* Part 2 *)
 
-directions = 
-  Thread[{0, 1, 2, 3} -> {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}];
-instructions = {
-     FromDigits[
-      toExpression[#[[3 ;; 7]]] /. 
-       Thread[CharacterRange["a", "f"] -> Range[10, 15]], 16],
-     ToExpression[#[[8]]] /. directions} & /@
-   
-   Characters[input[[;; , 3]]];
-
-trench = FoldList[#1 + Times @@ #2 &, {0, 0}, instructions];
-(Area[Polygon[trench]] + Perimeter[Polygon[trench]]/2 + 1)
+Sum[
+  VertexCount[g]-1-
+  VertexCount[FixedPoint[
+    VertexDelete[#,_?(Function[x,VertexInDegree[#,x]==0\[And]x!=-1])]&,
+    VertexDelete[g,v]]],
+  {v,VertexList[g][[2;;]]}]
